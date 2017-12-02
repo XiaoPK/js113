a4dca70a91c2eefa3ac2c7482fac742be01680bf
小皮卡 <515970376@qq.com>
2017-12-02 16:29:15 +0800

    2
    toEqual ！= equle

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,18 +4,18 @@ describe('taxi fee', function () {
     it('when the way is <= 2',function(){
         let distance = 1.5,waitTime = 0;
         let result = main(distance,waitTime);
-        expect(result).toEqule(6);
+        expect(result).toEqual(6);
     });
 
     it('whe the distance is 2-8',function(){
         let distance = 8,waitTime = 1;
         let result = main(distance,waitTime);
-        expect(result).toEqule(11);
+        expect(result).toEqual(11);
     })
 
     it('when the distance is > 8',function(){
         let distance = 10,waitTime = 1;
         let result = main(distance,waitTime);
-        expect(result).toEqule(13);
+        expect(result).toEqual(13);
     })
 });
